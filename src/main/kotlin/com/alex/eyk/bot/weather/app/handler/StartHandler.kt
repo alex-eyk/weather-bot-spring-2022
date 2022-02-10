9ad0eb5b59bd89629d81bec8a5ee1bae994aacff
aleksejkiselev <ximand931@gmail.com>
2022-02-10 17:06:59 +0300

    send simple reply

--- src/main/kotlin/com/alex/eyk/bot/weather/app/handler/StartHandler.kt
+++ src/main/kotlin/com/alex/eyk/bot/weather/app/handler/StartHandler.kt
@@ -4,11 +4,9 @@ import com.alex.eyk.bot.weather.core.entity.reply.impl.Replies
 import com.alex.eyk.bot.weather.core.entity.user.User
 import com.alex.eyk.bot.weather.core.entity.user.UserRepository
 import com.alex.eyk.bot.weather.core.handler.command.CommandHandler
-import com.alex.eyk.bot.weather.core.method.SendMessageBuilder
 import com.alex.eyk.dictionary.provider.DictionaryProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Message
 
@@ -20,20 +18,18 @@ class StartHandler @Autowired constructor(
     private val userRepository: UserRepository
 ) : CommandHandler(COMMAND) {
 
-    override fun saveHandle(user: User, message: Message): BotApiMethod<*> {
-        return SendMessageBuilder()
-            .chat(user.chat)
-            .reply(dictProvider.reply(user.languageCode, Replies.START))
-            .build()
+    override fun saveHandle(user: User, message: Message): SendMessage {
+        return super.sendSimpleReply(
+            user, dictProvider.reply(user.languageCode, Replies.START)
+        )
     }
 
     override fun notRegisteredHandle(message: Message): SendMessage {
         val user = User(message.chatId.toLong(), dictProvider.getDefaultLanguageCode())
         userRepository.save(user)
-        return SendMessageBuilder()
-            .chat(user.chat)
-            .reply(dictProvider.reply(user.languageCode, Replies.START_FIRST_TIME))
-            .build()
+        return super.sendSimpleReply(
+            user, dictProvider.reply(user.languageCode, Replies.START_FIRST_TIME)
+        )
     }
 
 }
--- src/main/kotlin/com/alex/eyk/bot/weather/core/handler/command/CommandHandler.kt
+++ src/main/kotlin/com/alex/eyk/bot/weather/core/handler/command/CommandHandler.kt
@@ -1,7 +1,20 @@
 package com.alex.eyk.bot.weather.core.handler.command
 
+import com.alex.eyk.bot.weather.core.entity.user.User
 import com.alex.eyk.bot.weather.core.handler.AbstractHandler
+import com.alex.eyk.bot.weather.core.method.SendMessageBuilder
+import com.alex.eyk.dictionary.Reply
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 
 abstract class CommandHandler(
     val command: String
-) : AbstractHandler()
+) : AbstractHandler() {
+
+    protected fun sendSimpleReply(user: User, reply: Reply): SendMessage {
+        return SendMessageBuilder()
+            .chat(user.chat)
+            .reply(reply)
+            .build()
+    }
+
+}
